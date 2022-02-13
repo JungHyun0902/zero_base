aea0ab90eb4234b0a0cc3ef778b63f8ecd545b50
JungHyun0902 <kimjh5354@gmail.com>
2022-02-13 22:51:24 +0900

    ch12_output

--- JS/12.js
+++ JS/12.js
@@ -2,12 +2,12 @@ let num_1, num_2, result_1, result_2;
 num_1 = 10;
 result_1 = num_1++;
 
-console.log(result_1);
-console.log(num_1);
+console.log(result_1); // 10
+console.log(num_1); // 11
 
 num_2 = 10;
 result_2 = --num_2;
 
-console.log(result_2);
-console.log(num_2);
+console.log(result_2); // 9
+console.log(num_2); // 9
 
