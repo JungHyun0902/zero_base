188401e4bd1456289d51a06e6498c6a16b362f64
JungHyun0902 <kimjh5354@gmail.com>
2022-02-21 17:25:08 +0900

    43.별별별 문제(반복문)

---
+++ JS/43_pr.js
@@ -0,0 +1,29 @@
+/*** 1. 별별별 ***/
+
+/* user code */
+function answer(num) {
+  let result = "";
+
+  // 코드 구현 시작 영역
+  for (let i = 0; i < num; i++) {
+    result += '*';
+  }
+// result = '*'.repeat(num); 도 가능 
+  // 코드 구현 종료 영역
+
+  return result;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  5,
+  // TC: 2
+  7,
+  // TC: 3
+  12,
+];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(`#${i + 1} ${answer(input[i])}`);
+}
