3f92824b53657bd77a473809ae3c04fc0a390d97
JungHyun0902 <kimjh5354@gmail.com>
2022-02-23 15:55:33 +0900

    50.윤년판별기

--- JS/50_pr.js
+++ JS/50_pr.js
@@ -6,7 +6,7 @@ function answer(year) {
 
   // 코드 구현 시작 영역
   
-  if((year % 4 == 0)&(year % 100 != 0)) {
+  if(year % 4 == 0 && year % 100 != 0) {
     result = true;
   } else if(year % 400 == 0) {
     result = true;
