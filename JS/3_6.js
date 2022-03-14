f686b095b3e7ab6102c03c53e3df4d4c46f20b3a
JungHyun0902 <kimjh5354@gmail.com>
2022-03-14 23:22:14 +0900

    3_6 re

--- JS/3_6.js
+++ JS/3_6.js
@@ -6,31 +6,55 @@ function answer(dwarf) {
 
   // 코드 구현 시작 영역
 
+  // let sum = 0;
+  // for(let i = 0; i < dwarf.length; i++) {
+  //   sum += dwarf[i];
+  // }
+  // sum -= 100;
+
+  // let fake_dwarf = [];
+  // for(let i = 0; i < dwarf.length; i++) {
+  //   for(let j = i + 1; j < dwarf.length; j++) {
+  //     if(dwarf[i] + dwarf[j] == sum) {
+  //       fake_dwarf[0] = i;
+  //       fake_dwarf[1] = j;
+  //       break;
+  //     }
+  //   }
+  //   if(fake_dwarf.length != 0) break;
+  // }
+
+  // let count = 0;
+  // for(let i = 0; i < dwarf.length; i++) {
+  //   if(fake_dwarf[0] != i && fake_dwarf[1] != i) {
+  //     result[count++] = dwarf[i];
+  //   }
+  // }
+
   let sum = 0;
   for(let i = 0; i < dwarf.length; i++) {
     sum += dwarf[i];
   }
   sum -= 100;
 
-  let fake_dwarf = [];
-  for(let i = 0; i < dwarf.length; i++) {
-    for(let j = i + 1; j < dwarf.length; j++) {
-      if(dwarf[i] + dwarf[j] == sum) {
-        fake_dwarf[0] = i;
-        fake_dwarf[1] = j;
+  let fake = []
+  for (let i = 0; i < dwarf.length; i++) {
+    for (let j = i + 1; j < dwarf.length; j++) {
+      if (dwarf[i] + dwarf[j] == sum) {
+        fake[0] = i;
+        fake[1] = j;
         break;
-      }
+      } 
     }
-    if(fake_dwarf.length != 0) break;
+    if (fake.length != 0) break;
   }
 
   let count = 0;
-  for(let i = 0; i < dwarf.length; i++) {
-    if(fake_dwarf[0] != i && fake_dwarf[1] != i) {
+  for (let i = 0; i < dwarf.length; i++) {
+    if (fake[0] != i && fake[1] != i) {
       result[count++] = dwarf[i];
     }
   }
-
   // 코드 구현 종료 영역
 
   return result;
