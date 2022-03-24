699b36c1c1b87d02950d1e1955aca933bb3b4c6c
JungHyun0902 <kimjh5354@gmail.com>
2022-03-24 18:58:11 +0900

    3_21. 이중 연결리스트 구현(1,2)

---
+++ JS/3_21.js
@@ -0,0 +1,91 @@
+// 이중 연결 리스트(1,2)
+
+// Node(): data와 point인 next, prev를 가진 객체
+function Node(data) {
+  this.data = data;
+  this.next = null;
+  this.prev = null;
+}
+
+// DoubleLinkedList(): head, tail과 length를 가진 객체
+function DoubleLinkedList() {
+  this.head = null;
+  this.tail = null;
+  this.length = 0;
+}
+
+// size(): 연결리스트 내 노드 개수 확인
+DoubleLinkedList.prototype.size = function() {
+  return this.length;
+};
+
+// isEmpty(): 객체 내 노드 존재 여부 파악
+DoubleLinkedList.prototype.isEmpty = function() {
+  return this.length === 0;
+};
+
+// printNode(): 노드 정방향 출력
+DoubleLinkedList.prototype.printNode = function() {
+  process.stdout.write('head -> ');
+  for(let node = this.head; node != null; node = node.next) {
+    process.stdout.write(`${node.data} -> `);
+  }
+  console.log('null');
+};
+
+// printNodeInverse(): 노드 역방향 출력
+DoubleLinkedList.prototype.printNodeInverse = function() {
+  let temp = [];
+
+  process.stdout.write('null <- ');
+  for(let node = this.tail; node != null; node = node.prev) {
+    temp.push(node.data);
+  }
+  for(let i = temp.length - 1; i >= 0; i--) {
+    process.stdout.write(`${temp[i]} <- `);
+  }
+  console.log('tail');
+};
+
+// append(): 연결리스트 가장 끝에 노드 추가
+DoubleLinkedList.prototype.append = function(value) {
+  let node = new Node(value);
+
+  if(this.head === null) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+  }
+
+  this.length++;
+};
+
+
+
+// Test Code 
+let dll = new DoubleLinkedList();
+// let node;
+// console.log(dll);
+
+// node = new Node(123);
+// dll.head = node;
+// dll.tail = node;
+// dll.length++;
+// console.log(dll);
+
+// node = new Node(456);
+// dll.tail.next = node;
+// node.prev = dll.tail;
+// dll.tail = node;
+// dll.length++;
+// console.log(dll);
+
+dll.append(1);
+dll.append(10);
+dll.append(100);
+
+dll.printNode();
+dll.printNodeInverse();
